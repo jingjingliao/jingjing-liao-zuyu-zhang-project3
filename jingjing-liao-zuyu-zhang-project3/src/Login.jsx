8a2d2c32d9c91ef43f532f2f5a3b3f821d6b6499
jingjingliao <55671806+jingjingliao@users.noreply.github.com>
2021-12-08 15:08:10 -0800

    update authentication css

--- jingjing-liao-zuyu-zhang-project3/src/Login.jsx
+++ jingjing-liao-zuyu-zhang-project3/src/Login.jsx
@@ -3,46 +3,53 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import React, { useState } from "react";
 
-export default function() {
+export default function () {
   const [UserLogginIn, setUserLogginIn] = useState({
     username: "",
-    password: ""
+    password: "",
   });
 
   return (
     <div class="login-page">
       <div class="form">
         <form class="login-form">
-          <input type="text" placeholder="username"
-          value={UserLogginIn.username}
-          onChange={(e) => {
-            const username = e.target.value;
-            setUserLogginIn(
-              {
+          <input
+            type="text"
+            placeholder="Username"
+            value={UserLogginIn.username}
+            onChange={(e) => {
+              const username = e.target.value;
+              setUserLogginIn({
                 ...UserLogginIn,
-                username: username
-              }
-            )
-          }}/>
-          <input type="password" placeholder="password"
-          value={UserLogginIn.password}
-          onChange={(e) => {
-            const password = e.target.value;
-            setUserLogginIn(
-              {
+                username: username,
+              });
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={UserLogginIn.password}
+            onChange={(e) => {
+              const password = e.target.value;
+              setUserLogginIn({
                 ...UserLogginIn,
-                password: password
-              }
-            )
-          }} type="password"/>
-          <button onClick={() => {
-                axios.post("http://localhost:8000/user/authenticate", UserLogginIn)
-                .then(response => setUserLogginIn(response.data))
-                .catch(error => console.log(error))
-          }}>Login</button>
+                password: password,
+              });
+            }}
+            type="password"
+          />
+          <button
+            onClick={() => {
+              axios
+                .post("http://localhost:8000/user/authenticate", UserLogginIn)
+                .then((response) => setUserLogginIn(response.data))
+                .catch((error) => console.log(error));
+            }}
+          >
+            Login
+          </button>
           <p class="message">
-            Not registered? 
-            <Link to="/signup">Create an account</Link>
+            Not registered? <Link to="/signup">Create an account</Link>
           </p>
         </form>
       </div>
