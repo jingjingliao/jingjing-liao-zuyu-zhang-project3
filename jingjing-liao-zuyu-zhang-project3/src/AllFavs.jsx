124b163a8b89e665e562f2225646933fbd3bc36e
isazyzhang <33339687+isazyzhang@users.noreply.github.com>
2021-12-12 14:59:15 -0800

    fav logics

--- jingjing-liao-zuyu-zhang-project3/src/AllFavs.jsx
+++ jingjing-liao-zuyu-zhang-project3/src/AllFavs.jsx
@@ -7,10 +7,10 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function AllFavs() {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     const [favs, setFavs] = useState([]);
+    const [job, setJob] = useState({});
     const [jobs, setJobs] = useState([]);
-    const [errorMsg, setError] = useState("");
+    const [error, setError] = useState("");
 
-    
     function favSearchResults() {
         axios
         .get("http://localhost:8000/user/findAllFavs/" + currentUser)
@@ -23,35 +23,67 @@ export default function AllFavs() {
     }
     useEffect(favSearchResults, []);
 
-    function getJobObjects() {
-        axios
-         .post("http://localhost:8000/job/getJobObjects", favs)
-         .then((response) => {
-             setJobs(response.data);
-         })
-         .catch((error) => {
-            setError("Failed to get job objects");
-        });
+    function getJobs() {
+        for (let fav of favs) {
+            axios
+            .get("http://localhost:8000/job/" + fav)
+            .then((response) => {
+                setJob(response.data);
+                jobs.push(job);
+            })
+            .catch((error) => {
+                setError("Failed to get job object");
+            });
+        }
+        setJobs(job);
     }
-    useEffect(getJobObjects, []);
+    useEffect(getJobs, []);
 
     return (
         <div>
             {jobs.map((job) => (
-                <div class="card">
-                    <div class="card-content">
-                        <span class="card-title">{job.jobTitle}</span>
-                        <p class="card-text">
-                            {job.location}
-                            <p>{job.companyName}</p>
-                        </p>
-                        <span class="card-link">
-                            <Link to={"/job/" + job._id}>More Details</Link>
-                        </span>
-                    </div>
-                    <img src={Image} />
+                <div>
+                    {job}
                 </div>
             ))}
         </div>
     )
+
+
+        // function getJobObjects() {
+    //     for (let fav of favs) {
+    //         console.log(fav);
+    //         axios
+    //             .get("http://localhost:8000/job/" + fav)
+    //             .then((response) => {
+    //                 jobArr.push(response.data);
+    //                 console.log(response.data);
+    //             })
+    //             .catch((error) => {
+    //                 setError("Failed to get job object");
+    //             });
+    //     }
+    //     setJobs(jobArr);
+    // }
+    // useEffect(getJobObjects, []);
+
+    // return (
+    //     <div>
+    //         {jobs.map((job) => (
+    //             <div class="card">
+    //                 <div class="card-content">
+    //                     <span class="card-title">{job.jobTitle}</span>
+    //                     <p class="card-text">
+    //                         {job.location}
+    //                         <p>{job.companyName}</p>
+    //                     </p>
+    //                     <span class="card-link">
+    //                         <Link to={"/job/" + job._id}>More Details</Link>
+    //                     </span>
+    //                 </div>
+    //                 <img src={Image} />
+    //             </div>
+    //         ))}
+    //     </div>
+    // )
 }
